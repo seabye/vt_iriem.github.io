8cbd2dde536471046a4efcd51927bc5833008335
seabye <seabye@live.com>
2020-10-15 02:59:04 +0800

    ...

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 'use strict';
-self.addEventListener('install',event=>event.waitUntil(caches.open('sw').then(cache=>cache.addAll([]))));
+self.addEventListener('install',event=>event.waitUntil(caches.open('').then(cache=>cache.addAll([]))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request))));
