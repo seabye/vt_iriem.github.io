cc42769d923bfd67c62eca0f26f2c8c9f58f3bf5
seabye <seabye@live.com>
2021-03-10 07:25:22 +0800

    ...

--- before.js
+++ before.js
@@ -13,7 +13,7 @@
         };
         window.addEventListener('touchstart',preventDefault,{passive:false});
         window.document.documentElement.style.setProperty('background-color',`${window.matchMedia('(prefers-color-scheme:dark)').matches?'#121212':'#FFFFFF'}`,'important');
-        window.document.documentElement.style.setProperty('position','relative');
+        window.document.documentElement.style.setProperty('position','fixed');
         window.document.documentElement.style.setProperty('width','100%');
         window.document.documentElement.style.setProperty('height','100%');
         window.document.head.insertAdjacentHTML('beforeend',`<meta name="viewport" content="width=device-width,user-scalable=no,viewport-fit=cover">`);
@@ -25,7 +25,7 @@
         const loop=()=>{
             if(window.document.body){
                 window.document.body.style.setProperty('background-color',`${window.matchMedia('(prefers-color-scheme:dark)').matches?'#121212':'#FFFFFF'}`,'important');
-                window.document.body.style.setProperty('position','relative');
+                window.document.body.style.setProperty('position','fixed');
                 window.document.body.style.setProperty('width','100%');
                 window.document.body.style.setProperty('height','100%');
                 // ~~~~
