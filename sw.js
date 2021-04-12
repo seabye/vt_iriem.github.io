62e33912cc2a3b1fbb869565e56605c92db187f3
seabye <seabye@live.com>
2021-04-12 16:23:26 +0800

    ...

--- sw.js
+++ sw.js
@@ -4,10 +4,41 @@
   self.console.log('#### start: sw.js');
 // #import
 // #variable
-// #method
+// #block
 // #build
+  // // service worker
+  // self.addEventListener('fetch',()=>{});
   // service worker
-  self.addEventListener('fetch',()=>{});
+  // const date=new self.Date();
+  // const version=`${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getMinutes()}`;
+  const version='auto';
+  self.addEventListener('install',(event)=>{
+    event.waitUntil(self.caches.open(version).then((cache)=>{
+      return cache.addAll([]);
+    }));
+  });
+  self.addEventListener('fetch',(event)=>{
+    event.respondWith(self.caches.match(event.request).then((respond)=>{
+      return self.fetch(event.request).then((response)=>{
+        const responseClone=response.clone();
+        self.caches.open(version).then((cache)=>{
+            cache.put(event.request,responseClone);
+        });
+        return response;
+      }).catch((error)=>{
+        return respond;
+      });
+    }));
+  });
+  self.addEventListener('activate',(event)=>{
+    event.waitUntil(self.caches.keys().then((keyList)=>{
+      return self.Promise.all(keyList.map((key)=>{
+        if([version].indexOf(key)===-1){
+          return self.caches.delete(key);
+        }
+      }));
+    }));
+  });
 // #debug
 // #after
   // console
