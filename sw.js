34e7fe7ac9ecb2e2948f2c3f714481a60776425a
seabye <seabye@live.com>
2021-05-01 10:21:50 +0800

    ...

--- sw.js
+++ sw.js
@@ -8,16 +8,16 @@
 // #build
   // online priority basic cache
   const version='basic';
-  self.addEventListener('install',(event)=>{
-    event.waitUntil(self.caches.open(version).then((cache)=>{
+  addEventListener('install',(event)=>{
+    event.waitUntil(caches.open(version).then((cache)=>{
       return cache.addAll([]);
     }));
   });
-  self.addEventListener('fetch',(event)=>{
-    event.respondWith(self.caches.match(event.request).then((resp)=>{
-      return self.fetch(event.request).then((response)=>{
+  addEventListener('fetch',(event)=>{
+    event.respondWith(caches.match(event.request).then((resp)=>{
+      return fetch(event.request).then((response)=>{
         const responseClone=response.clone();
-        self.caches.open(version).then((cache)=>{
+        caches.open(version).then((cache)=>{
           cache.put(event.request,responseClone);
         });
         return response;
@@ -26,11 +26,11 @@
       });
     }));
   });
-  self.addEventListener('activate',(event)=>{
-    event.waitUntil(self.caches.keys().then((keyList)=>{
-      return self.Promise.all(keyList.map((key)=>{
+  addEventListener('activate',(event)=>{
+    event.waitUntil(caches.keys().then((keyList)=>{
+      return Promise.all(keyList.map((key)=>{
         if([version].indexOf(key)===-1){
-          return self.caches.delete(key);
+          return caches.delete(key);
         }
       }));
     }));
